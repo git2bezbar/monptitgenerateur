f95050f7afb8ff94efdcaf2d650f7c7a41aec0d6
Adem <ademduran6888@gmail.com>
2024-04-05 15:36:22 +0200

    feat: making the generation work

---
+++ src/data/names.ts
@@ -0,0 +1,13 @@
+export const feminineBeginning = "maptit";
+
+export const masculineBeginning = "monptit"; 
+
+export const feminineNouns: string[] = [
+  "voiture", "chaise", "maison", "table", "tasse", "fleur", "robe", "chaussure", "fenêtre", "porte",
+  "pomme", "orange", "banane", "boisson", "bille", "encre", "souris", "clé", "télé", "balle"
+];
+
+export const masculineNouns: string[] = [
+  "chat", "chien", "rat", "poisson", "oiseau", "singe", "éléphant", "tigre", "lion", "ours",
+  "panda", "coq", "poulet", "cheval", "boutdeshit", "mouton", "vampire", "lapin", "renard", "loup"
+];
